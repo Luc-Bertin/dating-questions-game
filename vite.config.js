843a63f6e2d5040aa7989816a0eca3211755f6f5
Luc BERTIN <dev@lucbertin.com>
2023-08-24 17:11:28 +0200

    modifying base subpaths with respect to expected subpath set on github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // paths to assets are not correct for GitHub Pages deployment. This is a common issue when deploying to GitHub Pages, as project is hosted under a subpath, and all the asset paths need to be relative to that subpath
+  base: "/dating-questions-game/",
+});
