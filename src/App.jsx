478628aab3a27ecdc6f21cfb601300946c538503
Luc BERTIN <dev@lucbertin.com>
2023-08-25 16:57:26 +0200

    taking off questions as they are picked so they don't appear twice

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./components/Card";
 
-const questions = [
+const initialQuestions = [
   "Would you like to be famous? In what way?",
   "Do you often record again 10 times the same whatsapp audio before sending it ?",
   "What would constitute a “perfect” day for you?",
@@ -16,15 +16,40 @@ const questions = [
   "What's an issue you deal with every day basis?",
   "When did you last cry in front of another person? By yourself?",
   "Cat or Dog person?",
+  "Who's the person you tell everything about ?",
+  "If you were to write about someone who would it be?",
+  "What are the nicknames people call you at home/school/work?",
+  "What do you think falling in love feels like?",
+  "In friendship or love: do you prefer to give or receive?",
+  "What good things happened to you this summer?",
+  "Where do you see yourself in 5 years?",
 ];
 
 const App = () => {
+  const [questions, setQuestions] = useState(initialQuestions);
   const [questionIndex, setQuestionIndex] = useState(0);
 
   const pickRandomQuestion = () => {
-    const randomIndex = Math.floor(Math.random() * questions.length);
+    // Make a copy of the current questions except the one being removed
+    const newQuestions = questions.filter(
+      (_, index) => index !== questionIndex
+    );
+
+    // Pick a random index from the updated questions list
+    const randomIndex = Math.floor(Math.random() * newQuestions.length);
+
+    setQuestions(newQuestions);
     setQuestionIndex(randomIndex);
   };
+
+  useEffect(() => {
+    console.log("total questions:", questions.length);
+    if (questions.length === 0) {
+      // Handle the case when no questions are left, like resetting the questions
+      setQuestions(initialQuestions);
+      setQuestionIndex(Math.floor(Math.random() * initialQuestions.length));
+    }
+  }, [questions]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen w-full bg-gradient-to-r from-blue-500 to-green-300">
